a6ac6681cb3e692fe6dd1615a9941813be93e1cc
Matthew <12hughesm@gmail.com>
2023-10-31 18:22:57 +0000

    Completed challenge 5. Added input validation to challenge 4

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const convertDaysToAge = (days) => {
 
 // Challenge 3
 const getLargestNumber = (num1, num2, num3) => {
-    if (!(isNumber(num1)) || !(isNumber(num2)) || !(isNumber(num3)))
+    if (!isNumber(num1) || !isNumber(num2) || !isNumber(num3))
         throw "Invalid arguments";
 
     return Math.max(num1, num2, num3);
@@ -27,19 +27,37 @@ const getLargestNumber = (num1, num2, num3) => {
 
 // Challenge 4
 const getLastName = (names) => {
-
+    if(!isArrayOfStrings(names))
+        throw "Invalid input";
+    
     lastIndex = names.length - 1;
     return names[lastIndex];
 }
+
+// Challenge 5
+const allNumbersPositive = (numbers) => {
+    for(num of numbers){
+        if(num < 0) 
+            return false;
+    }
+
+    return true;
+} 
+
+console.log(allNumbersPositive([5, -10, 15, 20]));
+
 
 // input validation function
 function isNumber(input) {
     return (typeof input === 'number');
 }
 
-// input validation function
-function isString(input){
-    return (typeof input === 'string');
+function isArrayOfStrings(input){
+    for(i of input){
+        if(!(typeof i === 'string'))
+            return false;
+    }
+    return true;
 }
 
 
